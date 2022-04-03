b006782961e98afa804bfc10fbd96c38e7fb66b1
RoniRio <ronny_rio@hotmail.com>
2022-04-03 11:38:48 +0200

    hector

--- src/main/java/com/springsimplespasos/universidad/universidadbackend/UniversidadBackendApplication.java
+++ src/main/java/com/springsimplespasos/universidad/universidadbackend/UniversidadBackendApplication.java
@@ -15,29 +15,18 @@ import java.util.List;
 @SpringBootApplication
 public class UniversidadBackendApplication {
 
-    @Autowired
-    private AlumnoDAO servicio;
 
-    public static void main(String[] args) {
-        String[] beanDefinitionNames = SpringApplication.run(UniversidadBackendApplication.class, args).getBeanDefinitionNames();
+	@Autowired
+	private AlumnoDAO servicio;
+
+
+	public static void main(String[] args) {
+		String[] beanDefinitionNames = SpringApplication.run(UniversidadBackendApplication.class, args).getBeanDefinitionNames();
 		/*for(String str : beanDefinitionNames){
 			System.out.println(str);
 		}*/
-    }
-        @Bean
-        public CommandLineRunner runner () {
-            return args -> {
-                Direccion direccion = new Direccion("calle falete", "123", "3345", "", "", "Oviedo");
-                Persona alumno = new Alumno(null, "Juan", "Peréz", "958878585", direccion);
-
-                Persona save = servicio.save(alumno);
-                System.out.println(save.toString());
-
-
-            };
-
-        }
-
-
-
+	}
 }
+
+
+
--- src/test/java/com/springsimplespasos/universidad/universidadbackend/repositorios/CarreraRepositoryTest.java
+++ src/test/java/com/springsimplespasos/universidad/universidadbackend/repositorios/CarreraRepositoryTest.java
@@ -3,6 +3,9 @@ package com.springsimplespasos.universidad.universidadbackend.repositorios;
 import com.springsimplespasos.universidad.universidadbackend.datos.DatosDummy;
 import com.springsimplespasos.universidad.universidadbackend.modelo.entidades.Carrera;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
@@ -11,10 +14,10 @@ import java.util.List;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
-@DataJpaTest
+@ExtendWith(MockitoExtension.class)
 class CarreraRepositoryTest {
 
-    @Autowired
+    @Mock
     CarreraRepository carreraRepository;
 
     @BeforeEach
@@ -33,12 +36,11 @@ class CarreraRepositoryTest {
     @DisplayName("Buscar Carreras por nombre")
     void findCarrerasByNombreContains() {
         //given
-        /*carreraRepository.save(DatosDummy.carrera01());
-        carreraRepository.save(DatosDummy.carrera02());
-        carreraRepository.save(DatosDummy.carrera03());*/
+        carreraRepository.save(DatosDummy.carrera01(true));
+        carreraRepository.save(DatosDummy.carrera03(true));
 
         //when
-        Iterable<Carrera> expected = carreraRepository.findCarrerasByNombreContains("Sistemas");
+        Iterable<Carrera> expected = carreraRepository.findAll();
 
         //then
         assertThat(((List<Carrera>)expected).size() == 2).isTrue();
